3d1cbbec048c7d8ffee58cb22174b6827b90d6b6
Zero-Bits <charles.boehme@protonmail.com>
2018-10-03 13:27:29 -0500

    Began referencing RS instead of BS12

--- test/run-test.sh
+++ test/run-test.sh
@@ -241,11 +241,11 @@ function run_byond_tests {
         ./install-byond.sh || exit 1
         source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
     fi
-    run_test_ci "check globals build" "python tools/GenerateGlobalVarAccess/gen_globals.py baystation12.dme code/_helpers/global_access.dm"
+    run_test_ci "check globals build" "python tools/GenerateGlobalVarAccess/gen_globals.py redshift.dme code/_helpers/global_access.dm"
     run_test "check globals unchanged" "md5sum -c - <<< 'a3bfcbd4e5e35726b1ba5c6dec7c5e09 *code/_helpers/global_access.dm'"
-    run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH baystation12.dme"
+    run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH redshift.dme"
     run_test "check no warnings in build" "grep ', 0 warnings' build_log.txt"
-    run_test "run unit tests" "DreamDaemon baystation12.dmb -invisible -trusted -core 2>&1 | tee log.txt"
+    run_test "run unit tests" "DreamDaemon redshift.dmb -invisible -trusted -core 2>&1 | tee log.txt"
     run_test "check tests passed" "grep 'All Unit Tests Passed' log.txt"
     run_test "check no runtimes" "grep 'Caught 0 Runtimes' log.txt"
     run_test_fail "check no runtimes 2" "grep 'runtime error:' log.txt"
